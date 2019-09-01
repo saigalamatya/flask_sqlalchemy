f9a1d417b9861473a888a7b8d265a2bb89660eaa
saigal_amatya <saigal@noveltytechnology.com>
2019-09-01 16:57:42 +0545

    update route

--- app.py
+++ app.py
@@ -73,6 +73,26 @@ def get_product(id):
   
   return product_schema.jsonify(product)
 
+# Update a Product
+@app.route('/product/<int:id>', methods=['PUT'])
+def update_product(id):
+  product = Product.query.get(id)
+    
+  name = request.json['name']
+  description = request.json['description']
+  price = request.json['price']
+  qty = request.json['qty']
+
+  product.name = name
+  product.description = description
+  product.price = price
+  product.qty = qty
+
+  db.session.commit()
+
+  return product_schema.jsonify(product)
+
+
 # Run Server
 if __name__ == '__main__':
   app.run(debug=True)
